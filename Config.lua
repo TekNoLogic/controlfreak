e7a4a190a7b05236da4680e9ab0649a93f41e1a0
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-02-18 02:37:47 -0700

    Moved config frames to new Blizzy GUI, basic stuff now, more to come

--- Config.lua
+++ Config.lua
@@ -1,126 +1,164 @@
 ﻿
+
 if not ControlFreak then return end
 
 
-----------------------------
---      Localization      --
-----------------------------
-
-local L = CONTROLFREAK_LOCALE
-CONTROLFREAK_LOCALE = nil
-
-
--- Size: 630 305
--- Offset: 190 -103
-
+----------------------
+--      Locals      --
+----------------------
 
 local ww = LibStub("WidgetWarlock-Alpha1")
+local GAP = 10
 
+---------------------
+--      Panel      --
+---------------------
 
-function ControlFreak:CreatePanel()
-	local frame = CreateFrame("Frame", "ControlFreakFrame", UIParent)
-	local name = "ControlFreakFrame"
+local frame = CreateFrame("Frame")
+frame.name = "Control Freak"
+frame:Hide()
+ControlFreak.configframe = frame
+frame:SetScript("OnShow", function(frame)
+	local ControlFreak = ControlFreak
 
-
-	local displaygrouplabel = ww:SummonFontString(frame, "OVERLAY", "GameFontHighlightSmall", "Display Options", "TOPLEFT", frame, "TOPLEFT", 20, -5)
-	local displaygroup = ww:SummonGroupBox(frame, 305, 75, "TOPLEFT", displaygrouplabel, "BOTTOMLEFT", -15, 0)
-
-
-	local lockpos = ww:SummonCheckBox(displaygroup, 22, "TOPLEFT", 10, -5)
+	local lockpos = ww:SummonCheckBox(frame, 22, "TOPLEFT", GAP, -GAP)
 	ww:EnslaveLabel(lockpos, "Lock frame")
 	lockpos:SetHitRectInsets(0, -100, 0, 0)
 	ww:EnslaveTooltip(lockpos, "Locks the frame to prevent accidental movement")
-	lockpos:SetScript("OnClick", function() self.db.char.frameopts.locked = not self.db.char.frameopts.locked end)
-	lockpos:SetChecked(self.db.char.frameopts.locked)
+	lockpos:SetScript("OnClick", function() ControlFreak.db.char.frameopts.locked = not ControlFreak.db.char.frameopts.locked end)
+	lockpos:SetChecked(ControlFreak.db.char.frameopts.locked)
 
 
-	local showtip = ww:SummonCheckBox(displaygroup, 22, "TOPLEFT", lockpos, "TOPLEFT", 150, 0)
+	local showtip = ww:SummonCheckBox(frame, 22, "TOPLEFT", lockpos, "BOTTOMLEFT", 0, -GAP)
 	showtip:SetHitRectInsets(0, -100, 0, 0)
 	ww:EnslaveLabel(showtip, "Show tooltip")
 	ww:EnslaveTooltip(showtip, "Show help tooltip on hover")
-	showtip:SetScript("OnClick", function() self.db.char.showtooltip = not self.db.char.showtooltip end)
-	showtip:SetChecked(self.db.char.showtooltip)
+	showtip:SetScript("OnClick", function() ControlFreak.db.char.showtooltip = not ControlFreak.db.char.showtooltip end)
+	showtip:SetChecked(ControlFreak.db.char.showtooltip)
 
 
-	local threshslider, threshslidertext = ww:SummonSlider(frame, "Break Threshold: "..self.db.char.breakthreshold.." sec", 0, 10, "TOPLEFT", lockpos, "BOTTOMLEFT", 5, -15)
+	local threshslider, threshslidertext, threashsliderlow = ww:SummonSlider(frame, "Break Threshold: "..ControlFreak.db.char.breakthreshold.." sec", 0, 10, "TOPLEFT", frame, "TOP", GAP, -GAP-10)
 	ww:EnslaveTooltip(threshslider, "Time (in seconds) before spell breaks to unfade frame.")
-	threshslider:SetValue(self.db.char.breakthreshold)
+	threshslider:SetValue(ControlFreak.db.char.breakthreshold)
 	threshslider:SetValueStep(1)
 	threshslider:SetScript("OnValueChanged", function()
-		self.db.char.breakthreshold = threshslider:GetValue()
-		threshslidertext:SetText("Break Threshold: "..self.db.char.breakthreshold.." sec")
+		ControlFreak.db.char.breakthreshold = threshslider:GetValue()
+		threshslidertext:SetText("Break Threshold: "..ControlFreak.db.char.breakthreshold.." sec")
 	end)
 
 
-	local alpha = math.floor(self.db.char.alpha*100 + .5)
-	local alphaslider, alphaslidertext = ww:SummonSlider(frame, "Alpha: "..alpha.."%", "0%", "100%", "LEFT", threshslider, "RIGHT", 15, 0)
+	local alpha = math.floor(ControlFreak.db.char.alpha*100 + .5)
+	local alphaslider, alphaslidertext, alphasliderlow = ww:SummonSlider(frame, "Alpha: "..alpha.."%", "0%", "100%", "TOP", threashsliderlow, "BOTTOM", 0, -GAP-10)
+	alphaslider:SetPoint("LEFT", threshslider, "LEFT")
 	ww:EnslaveTooltip(alphaslider, "Alpha level to fade frame to when focus is controlled, dead, or not set.")
-	alphaslider:SetValue(self.db.char.alpha)
+	alphaslider:SetValue(ControlFreak.db.char.alpha)
 	alphaslider:SetValueStep(0.05)
 	alphaslider:SetScript("OnValueChanged", function()
-		self.db.char.alpha = alphaslider:GetValue()
-		local alpha = math.floor(self.db.char.alpha*100 + .5)
+		ControlFreak.db.char.alpha = alphaslider:GetValue()
+		local alpha = math.floor(ControlFreak.db.char.alpha*100 + .5)
 		alphaslidertext:SetText("Alpha: "..alpha.."%")
-		self:OnUpdate(true)
+		ControlFreak:OnUpdate(true)
 	end)
 
 
-	local debufflabel = ww:SummonFontString(frame, "OVERLAY", "GameFontNormalSmall", "Debuff", "TOPLEFT", displaygroup, "BOTTOMLEFT", 5, -10)
-	local debuff = ww:SummonEditBox(frame, 200, "LEFT", debufflabel, "RIGHT", 10, 0)
-	ww:EnslaveTooltip(debuff, "Debuffs to track for control.  Separate multiple debuffs with commas.")
-	debuff:SetScript("OnEditFocusLost", function()
-		self.db.profile.spellname = debuff:GetText()
-		self:ParseDebuffs(string.split(",", self.db.profile.spellname))
-	end)
-	debuff:SetScript("OnEscapePressed", function(self) self:ClearFocus() end)
+	--  DB  --
+	local currentgroup = ww:SummonGroupBox(frame, nil, 105, "TOP", alphasliderlow, "BOTTOM", 0, -GAP-10)
+	currentgroup:SetPoint("LEFT", GAP, 0)
+	currentgroup:SetPoint("RIGHT", -GAP, 0)
+	local currentlabel = ww:SummonFontString(currentgroup, "OVERLAY", "GameFontNormal", "Current profile:", "TOPLEFT", 10, 15)
+	local current = ww:SummonFontString(frame, "OVERLAY", "GameFontHighlight", ControlFreak.db:GetCurrentProfile(), "LEFT", currentlabel, "RIGHT", 10, 0)
 
+	local profiles = ControlFreak.db:GetProfiles()
 
-	local editbox = CreateFrame("EditBox", nil, frame)
-	editbox:SetWidth(620)
-	editbox:SetPoint("BOTTOMLEFT", 5, 5)
-	editbox:SetPoint("TOP", frame, "BOTTOM", 0, 170)
-	editbox:SetFontObject(GameFontHighlight)
-	editbox:SetTextInsets(8,8,8,8)
-	editbox:SetBackdrop(ww.TooltipBorderBG)
-	editbox:SetBackdropColor(.1,.1,.1,.3)
-	editbox:SetMultiLine(true)
-	editbox:SetAutoFocus(false)
-	editbox:SetScript("OnEditFocusLost", function()
-		self.db.profile.macrotext = editbox:GetText()
-		self.macroupdated = true
-		if not InCombatLockdown() then self:PLAYER_REGEN_ENABLED() end
-	end)
-	editbox:SetScript("OnEscapePressed", function(self) self:ClearFocus() end)
-	local macrolabel = ww:EnslaveLabel(editbox, "Macro", "BOTTOMLEFT", editbox, "TOPLEFT", 5, 0)
-	self.combatwarn = ww:EnslaveLabel(editbox, "|cffff0000Macro changes will not apply until combat ends!", "BOTTOMRIGHT", editbox, "TOPRIGHT", -5, 0)
-	if InCombatLockdown() then self.combatwarn:Show() else self.combatwarn:Hide() end
+	local selected = CreateFrame("Frame", "ControlFreakProfileMenu", currentgroup, "UIDropDownMenuTemplate")
+	local loadbutton = ww:SummonButton(currentgroup, "Load", nil, nil, "TOPLEFT", selected, "BOTTOMLEFT", 15, -5)
+	local copybutton = ww:SummonButton(currentgroup, "Copy", nil, nil, "LEFT", loadbutton, "RIGHT", 10, 0)
+	local deletebutton = ww:SummonButton(currentgroup, "Delete", nil, nil, "LEFT", copybutton, "RIGHT", 10, 0)
 
+	selected:SetPoint("TOPLEFT", -5, -10)
+	ControlFreakProfileMenuMiddle:SetWidth(250)
 
-	local checkgrouplabel = ww:SummonFontString(frame, "OVERLAY", "GameFontHighlightSmall", "Creature Types", "TOPLEFT", frame, "TOPLEFT", 330, -5)
-	local checkgroup = ww:SummonGroupBox(frame, 305, 102, "TOPLEFT", checkgrouplabel, "BOTTOMLEFT", -15, 0)
-
-
-	local a1, af, a2, dx, dy = "TOPLEFT", checkgroup, "TOPLEFT", 5, -5
-	local checks = {}
-	local creaturetypes = {L["Beast"], L["Demon"], L["Elemental"], L["Dragonkin"], L["Giant"], L["Humanoid"], L["Mechanical"], L["Undead"], L["Not specified"], L["Unknown"]}
-	for i,v in ipairs(creaturetypes) do
-		local check = ww:SummonCheckBox(checkgroup, 22, a1, af, a2, dx, dy)
-		checks[v] = check
-		ww:EnslaveLabel(check, v)
-		check:SetHitRectInsets(0, -100, 0, 0)
-		check:SetScript("OnClick", function() self.db.profile.targtypes[v] = not self.db.profile.targtypes[v] end)
-
-		if i == 5 then a1, af, a2, dx, dy = "TOPLEFT", checks.Beast, "TOPLEFT", 150, 0
-		else a1, af, a2, dx, dy = "TOPLEFT", check, "BOTTOMLEFT", 0, 4 end
+	local function ToggleButtons(value)
+		if value == ControlFreak.db:GetCurrentProfile() then
+			loadbutton:Disable()
+			copybutton:Disable()
+			deletebutton:Disable()
+		else
+			loadbutton:Enable()
+			copybutton:Enable()
+			deletebutton:Enable()
+		end
 	end
 
-	frame:SetScript("OnShow", function()
-		ww.FadeIn(frame, 0.5)
-		debuff:SetText(self.db.profile.spellname)
-		editbox:SetText(self.db.profile.macrotext or "/script ChatFrame1:AddMessage(\"Error loading macro!\")")
-		for i,v in ipairs(creaturetypes) do checks[v]:SetChecked(self.db.profile.targtypes[v]) end
+	local function DropdownClick()
+		UIDropDownMenu_SetSelectedValue(ControlFreakProfileMenu, this.value)
+		ToggleButtons(this.value)
+	end
+
+	local ddt = {func = DropdownClick}
+	local function DropdownInit()
+		local current = ControlFreak.db:GetCurrentProfile()
+		for i,v in ipairs(profiles) do
+			ddt.checked = false
+			ddt.text = v
+			ddt.value = v
+			ddt.disabled = v == current
+			UIDropDownMenu_AddButton(ddt)
+		end
+	end
+
+	selected:SetScript("OnShow", function(self)
+		UIDropDownMenu_Initialize(self, DropdownInit)
+		UIDropDownMenu_SetSelectedValue(selected, profiles[1])
+		ToggleButtons(profiles[1])
 	end)
 
-	return frame
-end
+	loadbutton:SetScript("OnClick", function()
+		local profile = UIDropDownMenu_GetSelectedValue(selected)
+		ControlFreak.db:SetProfile(profile)
+		current:SetText(profile)
+		ToggleButtons(profile)
+	end)
+	copybutton:SetScript("OnClick", function()
+		ControlFreak.db:ResetProfile()
+		ControlFreak.db:CopyProfile(UIDropDownMenu_GetSelectedValue(selected))
+		profiles = ControlFreak.db:GetProfiles()
+	end)
+	deletebutton:SetScript("OnClick", function()
+		ControlFreak.db:DeleteProfile(UIDropDownMenu_GetSelectedValue(selected))
+		profiles = ControlFreak.db:GetProfiles()
+		UIDropDownMenu_SetSelectedValue(selected, profiles[1])
+		ToggleButtons(profiles[1])
+	end)
 
+	local createname = ww:SummonEditBox(currentgroup, 187, "TOPLEFT", loadbutton, "BOTTOMLEFT", 8, 1)
+	local createbutton = ww:SummonButton(currentgroup, "Create", nil, nil, "LEFT", createname, "RIGHT", 5, -1)
+	createbutton:Disable()
+	createname:SetScript("OnTextChanged", function(frame) if frame:GetText() ~= "" then createbutton:Enable() else createbutton:Disable() end end)
+	createbutton:SetScript("OnClick", function()
+		local profile = createname:GetText()
+		if profile ~= "" then
+			local oldprofile = ControlFreak.db:GetCurrentProfile()
+			ControlFreak.db:SetProfile(profile)
+			ControlFreak.db:CopyProfile(oldprofile)
+			current:SetText(profile)
+			profiles = ControlFreak.db:GetProfiles()
+			createname:SetText("")
+			createname:ClearFocus()
+			createbutton:Disable()
+			ToggleButtons("")
+		end
+	end)
+
+	ww:EnslaveTooltip(copybutton, "Copy the selected profile into your current profile.")
+	ww:EnslaveTooltip(createbutton, "Duplicate the current profile into a new profile.")
+
+	local function OnShow(frame)
+		ww.FadeIn(frame, 0.5)
+		profiles = ControlFreak.db:GetProfiles()
+	end
+	frame:SetScript("OnShow", OnShow)
+	OnShow(frame)
+end)
+
+InterfaceOptions_AddCategory(frame)
---
+++ ConfigMacro.lua
@@ -0,0 +1,96 @@
+﻿
+if not ControlFreak then return end
+
+
+----------------------------
+--      Localization      --
+----------------------------
+
+local L = CONTROLFREAK_LOCALE
+CONTROLFREAK_LOCALE = nil
+
+
+----------------------
+--      Locals      --
+----------------------
+
+local ww = LibStub("WidgetWarlock-Alpha1")
+
+
+---------------------
+--      Panel      --
+---------------------
+
+local frame = CreateFrame("Frame")
+frame.parent = "Control Freak"
+frame.name = "Macro"
+ControlFreak.macroconfigframe = frame
+frame:Hide()
+frame:SetScript("OnShow", function(frame)
+	local ControlFreak = ControlFreak
+
+	local checkgrouplabel = ww:SummonFontString(frame, "OVERLAY", "GameFontHighlightSmall", "Creature Types", "TOPLEFT", frame, "TOPLEFT", 20, -10)
+	local checkgroup = ww:SummonGroupBox(frame, 305, 102, "TOPLEFT", checkgrouplabel, "BOTTOMLEFT", -15, 0)
+	checkgroup:SetPoint("RIGHT", frame, "RIGHT", -5, 0)
+
+
+	local a1, af, a2, dx, dy = "TOPLEFT", checkgroup, "TOPLEFT", 5, -5
+	local checks = {}
+	local creaturetypes = {L["Beast"], L["Demon"], L["Elemental"], L["Dragonkin"], L["Giant"], L["Humanoid"], L["Mechanical"], L["Undead"], L["Not specified"], L["Unknown"]}
+	for i,v in ipairs(creaturetypes) do
+		local check = ww:SummonCheckBox(checkgroup, 22, a1, af, a2, dx, dy)
+		checks[v] = check
+		ww:EnslaveLabel(check, v)
+		check:SetHitRectInsets(0, -100, 0, 0)
+		check:SetScript("OnClick", function() ControlFreak.db.profile.targtypes[v] = not ControlFreak.db.profile.targtypes[v] end)
+
+		if i == 5 then a1, af, a2, dx, dy = "TOPLEFT", checkgroup, "TOP", 5, -5
+		else a1, af, a2, dx, dy = "TOPLEFT", check, "BOTTOMLEFT", 0, 4 end
+	end
+
+
+	local debufflabel = ww:SummonFontString(frame, "OVERLAY", "GameFontNormalSmall", "Debuff", "TOPLEFT", checkgroup, "BOTTOMLEFT", 5, -10)
+	local debuff = ww:SummonEditBox(frame, 200, "LEFT", debufflabel, "RIGHT", 10, 0)
+	debuff:SetPoint("RIGHT", frame, "RIGHT", -10, 0)
+	ww:EnslaveTooltip(debuff, "Debuffs to track for control.  Separate multiple debuffs with commas.")
+	debuff:SetScript("OnEditFocusLost", function()
+		ControlFreak.db.profile.spellname = debuff:GetText()
+		ControlFreak:ParseDebuffs(string.split(",", ControlFreak.db.profile.spellname))
+	end)
+	debuff:SetScript("OnEscapePressed", debuff.ClearFocus)
+
+
+	local macrolabel = ww:SummonFontString(frame, "OVERLAY", "GameFontNormalSmall", "Macro", "LEFT", debufflabel, "LEFT")
+	macrolabel:SetPoint("TOP", debuff, "BOTTOM", 0, -5)
+	local editbox = CreateFrame("EditBox", nil, frame)
+	editbox:SetPoint("TOP", macrolabel, "BOTTOM", 0, -5)
+	editbox:SetPoint("LEFT", 5, 0)
+	editbox:SetPoint("BOTTOMRIGHT", -5, 5)
+	editbox:SetFontObject(GameFontHighlight)
+	editbox:SetTextInsets(8,8,8,8)
+	editbox:SetBackdrop(ww.TooltipBorderBG)
+	editbox:SetBackdropColor(.1,.1,.1,.3)
+	editbox:SetMultiLine(true)
+	editbox:SetAutoFocus(false)
+	editbox:SetScript("OnEditFocusLost", function()
+		ControlFreak.db.profile.macrotext = editbox:GetText()
+		ControlFreak.macroupdated = true
+		if not InCombatLockdown() then ControlFreak:PLAYER_REGEN_ENABLED() end
+	end)
+	editbox:SetScript("OnEscapePressed", editbox.ClearFocus)
+	ControlFreak.combatwarn = ww:EnslaveLabel(editbox, "|cffff0000Macro changes will not apply until combat ends!", "BOTTOMRIGHT", editbox, "TOPRIGHT", -5, 0)
+	if InCombatLockdown() then ControlFreak.combatwarn:Show() else ControlFreak.combatwarn:Hide() end
+
+
+	local function OnShow(frame)
+		ww.FadeIn(frame, 0.5)
+		debuff:SetText(ControlFreak.db.profile.spellname)
+		editbox:SetText(ControlFreak.db.profile.macrotext or "/script ChatFrame1:AddMessage(\"Error loading macro!\")")
+		for i,v in ipairs(creaturetypes) do checks[v]:SetChecked(ControlFreak.db.profile.targtypes[v]) end
+	end
+	frame:SetScript("OnShow", OnShow)
+	OnShow(frame)
+end)
+
+InterfaceOptions_AddCategory(frame)
+
