4a92fd614cda59acd21491662edb8359ffb17da1
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-16 18:37:58 -0600

    Add "About" panel and TOC metadata

--- Config.lua
+++ Config.lua
@@ -69,6 +69,9 @@ end)
 InterfaceOptions_AddCategory(frame)
 
 
+LibStub("tekKonfig-AboutPanel").new("Control Freak", "ControlFreak")
+
+
 -----------------------------
 --      Slash command      --
 -----------------------------
