388e8aa176ccdd80684faa40928ea94273b193ed
Tekkub <tekkub@gmail.com>
2008-10-14 17:44:53 -0600

    Add Russian locale

--- Locale.lua
+++ Locale.lua
@@ -51,6 +51,23 @@ if loc == "frFR" then localized = {
 } end
 
 
+-----------------------
+--      Russian      --
+-----------------------
+
+if loc == "ruRU" then localized = {
+	Beast = "\208\150\208\184\208\178\208\190\209\130\208\189\208\190\208\181",
+	Demon = "\208\148\208\181\208\188\208\190\208\189",
+	Elemental = "\208\173\208\187\208\181\208\188\208\181\208\189\209\130\208\176\208\187\209\140",
+	Dragonkin = "\208\148\209\128\208\176\208\186\208\190\208\189",
+	Giant = "\208\147\208\184\208\179\208\176\208\189\209\130",
+	Humanoid = "\208\147\209\131\208\188\208\176\208\189\208\190\208\184\208\180",
+	Mechanical = "\208\156\208\181\209\133\208\176\208\189\208\184\208\183\208\188",
+	Undead = "\208\157\208\181\208\182\208\184\209\130\209\140",
+	Unknown = "\208\157\208\181 \209\131\208\186\208\176\208\183\208\176\208\189\208\190",
+} end
+
+
 -- Metatable majicks... makes localized table fallback to engrish, or fallback to the index requested.
 -- This ensures we ALWAYS get a value back, even if it's the index we requested originally
 CONTROLFREAK_LOCALE = localized and setmetatable(localized, {__index = function(t,i) return engrish[i] or i end})
